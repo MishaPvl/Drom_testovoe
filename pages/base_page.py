ad713bc2260d48cd81d2b15ae19ad9180925b465
MishaPvl <godslayerepta@gmail.com>
2023-06-16 13:32:35 +0700

    Добавлены общие методы в файле base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,5 +5,17 @@ class BasePage():
     def __init__(self, driver):
         self.driver = driver
 
-    def enter_number(self, number):
-        pass
+    def find_element(self, locator):
+        return self.driver.find_element(*locator)
+
+    def enter_number(self, locator, number):
+        field = self.find_element(locator)
+        field.clear()
+        field.send_keys(number)
+
+    def click_button(self, locator):
+        button = self.find_element(locator)
+        button.click()
+
+    def get_text(self, locator):
+        text = self.find_element(locator).text
---
+++ pages/calculator_page.py
@@ -0,0 +1,6 @@
+from .base_page import BasePage
+from locators.calculator_locators import CalculatorLocators
+
+
+class CalculatorPage(BasePage):
+    pass
