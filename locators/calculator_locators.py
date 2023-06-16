6030c6b0ea316e21880590a58106834cf9b9145c
MishaPvl <godslayerepta@gmail.com>
2023-06-16 12:39:00 +0700

    С помощью Appium Inspector были выделены все локаторы в отдельный файл

---
+++ locators/calculator_locators.py
@@ -0,0 +1,9 @@
+class CalculatorLocators:
+    LEFT_INPUT_FIELD = '//android.widget.EditText[@content-desc="inputFieldLeft"]'
+    RIGHT_INPUT_FIELD = '//android.widget.EditText[@content-desc="inputFieldRight"]'
+    ADDITION_BUTTON = '//android.widget.Button[@content-desc="additionButton"]'
+    SUBTRACTION_BUTTON = '//android.widget.Button[@content-desc="subtractButton"]'
+    MULTIPLICATION_BUTTON = '//android.widget.Button[@content-desc="multiplicationButton"]'
+    DIVISION_BUTTON = '//android.widget.Button[@content-desc="divisionButton"]'
+    RESET_BUTTON = '//android.widget.Button[@content-desc="resetButton"]'
+    RESULT_TEXT = '//android.widget.TextView[@content-desc="resultTextView"]'
--- test_appium.py
+++ test_appium.py
@@ -20,7 +20,14 @@ def test_start():
             MobileBy.XPATH, '//android.widget.EditText[@content-desc="inputFieldLeft"]')
         digit1_btn.click()
         digit1_btn.send_keys(7)
-        time.sleep(10)
+        digit2_btn = driver.find_element(
+            MobileBy.XPATH, '//android.widget.EditText[@content-desc="inputFieldRight"]')
+        digit2_btn.click()
+        digit2_btn.send_keys(3)
+        first = driver.find_element(
+            MobileBy.XPATH, '//android.widget.Button[@content-desc="subtractButton"]')
+        first.click()
+        time.sleep(5)
 
     finally:
         driver.quit()
@@ -28,3 +35,12 @@ def test_start():
 
 if __name__ == '__main__':
     test_start()
+# LOCATORS
+# left_field = //android.widget.EditText[@content-desc="inputFieldLeft"]
+# right_field = //android.widget.EditText[@content-desc="inputFieldRight"]
+# plus = //android.widget.Button[@content-desc="additionButton"]
+# minus = //android.widget.Button[@content-desc="subtractButton"]
+# multipl = //android.widget.Button[@content-desc="multiplicationButton"]
+# division = //android.widget.Button[@content-desc="divisionButton"]
+# reset = //android.widget.Button[@content-desc="resetButton"]
+# result = //android.widget.TextView[@content-desc="resultTextView"]
