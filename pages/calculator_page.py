b1a77516f5c60bc46ddcbc052fbe281c7c455976
MishaPvl <godslayerepta@gmail.com>
2023-06-18 21:54:39 +0700

    Исправлены проверки функции сложения, часть функций вынесена в файл calculator_page

--- pages/calculator_page.py
+++ pages/calculator_page.py
@@ -27,3 +27,25 @@ class CalculatorPage(BasePage):
 
         second_value = self.find_element(CalculatorLocators.RIGHT_INPUT_FIELD).text
         assert second_value == '', 'Правое поле не пустое'
+
+    def perform_addition(self, left_operand, right_operand):
+        self.enter_number(CalculatorLocators.LEFT_INPUT_FIELD, left_operand)
+        self.enter_number(CalculatorLocators.RIGHT_INPUT_FIELD, right_operand)
+        self.click_addition_button()
+
+    def verify_result(self, expected_result):
+        result = self.get_result_after_equal_sign()
+        assert result == expected_result, f'Ожидаемый результат {expected_result} отличается от фактического {result}'
+
+    def verify_error_message(self, expected_error_message):
+        error_message = self.get_text(CalculatorLocators.RESULT_TEXT)
+        assert error_message == expected_error_message, f'Ожидаемое сообщение об ошибке {expected_error_message} не соответсвует фактическому {error_message}'
+
+    def get_result_after_equal_sign(self):
+        result = self.get_text(CalculatorLocators.RESULT_TEXT)
+        equal_sign_index = result.find('=')
+        if equal_sign_index != -1:
+            number_after_equal_sign = result[equal_sign_index + 1:].strip()
+            return number_after_equal_sign
+        else:
+            return None
